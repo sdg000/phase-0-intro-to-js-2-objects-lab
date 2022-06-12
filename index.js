5470827def59cf1322f675f1e84f07e65cc62b71
Seth David Gyimah <esec26@gmail.com>
2022-06-12 00:52:42 +0000

    phase 0 -intro to js2-objects lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "sam",
+    streetAddress: 1022
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newObject = {...employee, ...{[key]: value}}
+    //return Object[key] = value
+    return newObject
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    //return employee[name] = '12 Broadway'
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+
+    let newList = {...employee}
+    delete newList[key]
+    return newList
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
